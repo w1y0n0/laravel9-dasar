d08a0bc806c4ac955ad08d4a4d46a3d752fdf16e
w1y0n0 <wiyono30@gmail.com>
2023-07-04 16:19:02 +0700

    update view welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -449,7 +449,7 @@
 
                         <div class="ml-12">
                             <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                LabSI Project - Laravel 9
+                                LabSI Project - Laravel v9.x
                             </div>
                         </div>
                     </div>
